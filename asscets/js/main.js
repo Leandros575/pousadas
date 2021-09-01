c1fe8516342428b56ec0d5886c8a7f2c06109e4d
Leandro Camargo <77089638+Leandros575@users.noreply.github.com>
2021-09-01 19:07:03 -0300

    adicionando slider e fazendo css

--- asscets/js/main.js
+++ asscets/js/main.js
@@ -1,7 +1,5 @@
-$('.slider').slick({
-    dots: true,
+$('.slider-principal').slick({
     infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
+    slidesToShow: 3,
+    slidesToScroll: 3
 });
